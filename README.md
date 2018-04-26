a2591e285f25dd5c95067c65e6575881a842987c
DenSev <Den.Sev@outlook.com>
2018-04-26 17:43:17 +0300

    w/e

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # rustbucket
 
+resources:
+http://arewegameyet.com/
+
+libs:
 https://github.com/PistonDevelopers/graphics
+https://github.com/pistondevelopers/conrod
+
+games:
+https://github.com/Coeuvre/rust-2048
--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,20 @@
-use std::io;
-
 fn main() {
-    /*println!("Guess the number!");
+    let mut s = String::from("some words or whatever");
 
-    println!("Please input your guess.");
+    let us = first_word(&mut s);
 
-    let mut guess = String::new();
-
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
-
-    println!("You guessed: {}", guess);
-
-    another_function(10, 22);*/
-
-    poop();
+    println!("{}", us)
 }
 
-fn poop() {
-    let x = get_x();
 
-    let y = {
-        let x = 3;
-        x + 1
-    };
+fn first_word(s: &String) -> usize {
+    let bytes = s.as_bytes();
 
-    if x > 0 && y > 0 {
-        println!("yes")
-    } else {
-        println!("no")
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
     }
 
-    another_function(x, y);
-
-    let a = [10, 20, 30, 40, 50];
-
-    for element in a.iter() {
-        println!("the value is: {}", element);
-    }
-}
-
-fn another_function(x: i32, y: i32) {
-    println!("The value of x is: {}", x);
-    println!("The value of y is: {}", y);
-}
-
-fn get_x() -> i32 {
-    0
+    s.len()
 }
