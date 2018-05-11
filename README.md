057458e1e422ea02a28da76488076512f44a5602
DenSev <Den.Sev@outlook.com>
2018-05-11 18:03:32 +0300

    w/e

--- README.md
+++ README.md
@@ -7,5 +7,7 @@ libs:
 https://github.com/PistonDevelopers/graphics
 https://github.com/pistondevelopers/conrod
 
+https://github.com/hyperium/hyper
+
 games:
 https://github.com/Coeuvre/rust-2048
--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,26 @@ fn main() {
 
     let us = first_word(&mut s);
 
-    println!("{}", us)
+    println!("{}", us);
+
+    let user = User {
+        name: String::from("denis"),
+        age: 27,
+    };
+
+    let is_adult = user.is_adult();
+
+    print!("{} is adult? {}", user.name, is_adult.to_string())
 }
 
 
 fn first_word(s: &String) -> usize {
     let bytes = s.as_bytes();
+    let s2;
+    {
+        s2 = User::some_other_function();
+    }
+    println!("{}", s2);
 
     for (i, &item) in bytes.iter().enumerate() {
         if item == b' ' {
@@ -16,5 +30,21 @@ fn first_word(s: &String) -> usize {
         }
     }
 
-    s.len()
+    s2.len()
+}
+
+#[derive(Debug)]
+struct User {
+    name: String,
+    age: u32,
+}
+
+impl User {
+    fn is_adult(&self) -> bool {
+        return self.age >= 18;
+    }
+
+    fn some_other_function<'a>() -> &'a str {
+        return "tutturu";
+    }
 }
