1eb9fdf7b284002dee840482d4a4f3510cda6bb5
DenSev <den.sev@outlook.com>
2018-09-17 18:14:55 +0300

    updates

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
+use ::Sex::MALE;
+use std::env;
+use std::fmt::Display;
+
 fn main() {
+
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
     let mut s = String::from("some words or whatever");
 
     let us = first_word(&mut s);
@@ -8,13 +15,31 @@ fn main() {
     let user = User {
         name: String::from("denis"),
         age: 27,
+        sex: MALE
     };
 
     let is_adult = user.is_adult();
 
-    print!("{} is adult? {}", user.name, is_adult.to_string())
+    println!("{} is adult? {}", user.name, is_adult.to_string());
+    println!("{}", user);
+
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    println!("{}", largest(&number_list));
 }
 
+
+fn largest<T: PartialOrd>(list: &[T]) -> &T {
+    let mut largest = &list[0];
+
+    for item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
 
 fn first_word(s: &String) -> usize {
     let bytes = s.as_bytes();
@@ -33,10 +58,26 @@ fn first_word(s: &String) -> usize {
     s2.len()
 }
 
-#[derive(Debug)]
+//#[derive(Debug)]
+enum Sex {
+    MALE,
+    FEMALE
+}
+
+impl Display for Sex {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match &self {
+            Sex::MALE => write!(f, "male"),
+            Sex::FEMALE => write!(f, "female")
+        }
+    }
+}
+
+//#[derive(Debug)]
 struct User {
     name: String,
     age: u32,
+    sex: Sex
 }
 
 impl User {
@@ -46,5 +87,11 @@ impl User {
 
     fn some_other_function<'a>() -> &'a str {
         return "tutturu";
+    }
+}
+
+impl Display for User {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "name: {}\nage: {}\nsex: {}", &self.name, &self.age, &self.sex)
     }
 }
