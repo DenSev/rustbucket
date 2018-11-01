7c306341dc69962e1060418b23b7081103eae849
DenSev <den.sev@outlook.com>
2018-11-01 17:49:54 +0300

    updates

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,44 @@
 use std::env;
 use std::fs;
+use std::process;
+use std::error::Error;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
-    let content = fs::read_to_string(filename)
-        .expect("Something went wrong reading the file!");
-
-    println!("With text:\n{}", content)
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+        process::exit(1)
+    };
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let filename = &args[2];
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+    println!("With text:\n{}", contents);
 
-    return (query, filename);
+    Ok(())
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        return Ok(Config { query, filename });
+    }
 }
